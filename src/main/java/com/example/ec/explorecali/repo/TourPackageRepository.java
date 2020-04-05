edab6dc513e0290b0d5e8c03a14bfa15c7b3d217
wolfy027 <jomon027@gmail.com>
2020-04-05 20:31:24 +0530

    expose restful apis with spring mvc

--- src/main/java/com/example/ec/explorecali/repo/TourPackageRepository.java
+++ src/main/java/com/example/ec/explorecali/repo/TourPackageRepository.java
@@ -1,50 +1,11 @@
 package com.example.ec.explorecali.repo;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.ec.explorecali.model.TourPackage;
 
-@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends PagingAndSortingRepository<TourPackage, String> {
 	TourPackage findByName(String name);
 
-	@Override
-	@RestResource(exported = false)
-	default <S extends TourPackage> S save(S entity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	@RestResource(exported = false)
-	default <S extends TourPackage> Iterable<S> saveAll(Iterable<S> entities) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	@RestResource(exported = false)
-	default void deleteById(String id) {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	@RestResource(exported = false)
-	default void delete(TourPackage entity) {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	@RestResource(exported = false)
-	default void deleteAll(Iterable<? extends TourPackage> entities) {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	@RestResource(exported = false)
-	default void deleteAll() {
-		// TODO Auto-generated method stub
-	}
 }
